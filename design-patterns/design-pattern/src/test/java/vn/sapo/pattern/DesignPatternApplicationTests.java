55cff70aea673716e3f4bfb4ac61e003f6c5ac40
tuannt4 <tuannt4@sapo.vn>
2021-06-17 21:35:32 +0700

    project

---
+++ design-patterns/design-pattern/src/test/java/vn/sapo/pattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.sapo.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
